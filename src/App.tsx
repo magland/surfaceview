9280fe05b7b856c79d98a5b7a640f790185110de
Jeremy Magland <jeremy.magland@gmail.com>
2021-04-28 02:35:50 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import GoogleDriveFile from './components/google/GoogleDriveFile';
+import SelectFile from './components/SelectFile';
+import SurfaceViewFromFile from './components/SurfaceViewFromFile';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState<GoogleDriveFile | null>(null)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        selectedFile === null ? (
+          <SelectFile folderName={"surfaceview"} onFileSelected={setSelectedFile} />
+        ) : (
+          <SurfaceViewFromFile file={selectedFile} />
+        )
+      }
+      
     </div>
   );
 }
